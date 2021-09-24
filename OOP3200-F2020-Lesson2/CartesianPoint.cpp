d9852251564b63bcdcf84323043dc555e2606af5
Gaelen <gaelenrhoads@gmail.com>
2021-09-24 17:07:51 -0400

    Added operator<<, operator>> and operator= overloads

--- OOP3200-F2020-Lesson2/CartesianPoint.cpp
+++ OOP3200-F2020-Lesson2/CartesianPoint.cpp
@@ -4,7 +4,7 @@
 #include <cmath>			// sqrt()
 #include <limits>			// INT_MAX
 
-CartesianPoint::CartesianPoint(const int x, const int y): myX(x), myY(y)
+CartesianPoint::CartesianPoint(const int x, const int y): m_x(x), m_y(y)
 {
 	//SetPoint(x, y);
 }
@@ -20,8 +20,8 @@ CartesianPoint::CartesianPoint(const CartesianPoint& point2)
 CartesianPoint CartesianPoint::operator+(const CartesianPoint& point2) const
 {
 	CartesianPoint tempPoint;
-	tempPoint.SetX(GetX() + point2.GetX());
-	tempPoint.SetY(GetY() + point2.GetY());
+	tempPoint.SetX((*this).GetX() + point2.GetX());
+	tempPoint.SetY((*this).GetY() + point2.GetY());
 	
 	return tempPoint;
 }
@@ -29,10 +29,10 @@ CartesianPoint CartesianPoint::operator+(const CartesianPoint& point2) const
 double CartesianPoint::operator-(const CartesianPoint& point_to) const
 {
 	// difference between x values
-	const int xDelta = point_to.myX - myX;
+	const int xDelta = point_to.GetX() - m_x;
 
 	// difference between y values
-	const int yDelta = point_to.myY - myY;
+	const int yDelta = point_to.GetY() - m_y;
 
 	// return the formula (based on Pythagorean theorem)
 	return sqrt((xDelta * xDelta) + (yDelta * yDelta));
@@ -43,6 +43,29 @@ bool CartesianPoint::operator==(const CartesianPoint& other_point) const
 	return (GetX() == other_point.GetX()) && (GetY() == other_point.GetY());
 }
 
+CartesianPoint CartesianPoint::operator=(const CartesianPoint& new_point)
+{
+	SetX(new_point.GetX());
+	SetY(new_point.GetY());
+
+	return *this;
+}
+
+std::ostream& operator<<(std::ostream& out, const CartesianPoint& point)
+{
+	out << point.ToString();
+	return out;
+}
+
+std::istream& operator>>(std::istream& in, CartesianPoint& point)
+{
+	in >> point.m_x;
+	in.ignore();
+	in >> point.m_y;
+
+	return in;
+}
+
 void CartesianPoint::SetPoint(int x, int y)
 {
 	SetX(x);
@@ -51,22 +74,22 @@ void CartesianPoint::SetPoint(int x, int y)
 
 void CartesianPoint::SetX(int x)
 {
-	myX = x;
+	m_x = x;
 }
 
 void CartesianPoint::SetY(int y)
 {
-	myY = y;
+	m_y = y;
 }
 
 int CartesianPoint::GetX() const
 {
-	return myX;
+	return m_x;
 }
 
 int CartesianPoint::GetY() const
 {
-	return myY;
+	return m_y;
 }
 
 /** GetDistanceTo Method for CartesianPoint class
@@ -77,10 +100,10 @@ int CartesianPoint::GetY() const
 double CartesianPoint::GetDistanceTo(const CartesianPoint& point_to) const
 {
 	// difference between x values
-	const int xDelta = point_to.myX - myX;
+	const int xDelta = point_to.GetX() - m_x;
 
 	// difference between y values
-	const int yDelta = point_to.myY - myY;
+	const int yDelta = point_to.GetY() - m_y;
 
 	// return the formula (based on Pythagorean theorem)
 	return sqrt((xDelta * xDelta) + (yDelta * yDelta));
@@ -97,8 +120,10 @@ std::string CartesianPoint::ToString() const
 	std::stringstream strOut;
 
 	// build the string
-	strOut << "(" << myX << ", " << myY << ")";
+	strOut << "(" << m_x << ", " << m_y << ")";
 
 	// return the string
 	return strOut.str();
 }
+
+
--- OOP3200-F2020-Lesson2/CartesianPoint.h
+++ OOP3200-F2020-Lesson2/CartesianPoint.h
@@ -25,6 +25,12 @@ public:
 	double operator-(const CartesianPoint& point_to) const;
 	bool operator==(const CartesianPoint& other_point) const;
 
+	CartesianPoint operator=(const CartesianPoint& new_point);
+
+	/* friend overloads */
+	friend std::ostream& operator<<(std::ostream& out, const CartesianPoint& point);
+	friend std::istream& operator>>(std::istream& in, CartesianPoint& point);
+
 	/* Accessors: Used to query the state of the object */
 
 	// get x
@@ -54,8 +60,8 @@ public:
 
 private:
 	// private data members for the dimensions of the point
-	int myX{}; // x-axis (horizontal) value
-	int myY{};  // y-axis (vertical) value
+	int m_x; // x-axis (horizontal) value
+	int m_y;  // y-axis (vertical) value
 
 };
 

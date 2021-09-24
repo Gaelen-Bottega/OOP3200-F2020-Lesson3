ed92a61f247e9e8c42c86ed2e7ec5dd3bff92ec1
Gaelen <gaelenrhoads@gmail.com>
2021-09-23 22:24:49 -0400

    Add project files.

---
+++ OOP3200-F2020-Lesson2/main.cpp
@@ -0,0 +1,72 @@
+/** main.cpp
+ *
+ *	In this example program, did the following:
+ *		- Examined a simple class with 'instance' data-members, a constructor, accessors, mutators.
+ *
+ *	@author		Gaelen Rhoads
+ *	@studentID  100804776
+*/
+
+#include <iostream> 		// cin, cout
+#include <stdexcept>		// out_of_range
+#include <iomanip>			// fixed, setprecision()
+#include "MyConsoleInput.h" // ConsoleInput::ReadInteger()
+#include "CartesianPoint.h"
+
+// class declaration section
+
+
+// main() function
+int main()
+{
+	// Declarations
+	// a point called 'origin', initialized to 0, 0
+	CartesianPoint origin = CartesianPoint(0, 0);
+	
+	// a point called 'destination'
+	CartesianPoint destination;
+
+	int inputX; // temporary input variable for x
+	int inputY; // temporary input variable for y
+	
+	// to store the distance between two points
+	double distance;
+
+	try
+	{
+		// Input
+
+		// Prompt the user for the coordinates
+		std::cout << "\nEnter coordinates of the destination point: " << std::endl;
+		// prompt for, read, and store x coordinate:
+		std::cout << "X: ";
+		inputX = ConsoleInput::ReadInteger();
+		destination.SetX(inputX);
+
+		// prompt for, read, and store y coordinate:
+		std::cout << "Y: ";
+		inputY = ConsoleInput::ReadInteger();
+		destination.SetY(inputY);
+
+
+		// Processing
+		// determine the distance between the two points
+		distance = origin.GetDistanceTo(destination);
+
+		// Output 
+		// Show the points and the distance
+		std::cout << std::fixed << std::setprecision(3); // formatting
+		std::cout << "\nThe Distance between " << origin.ToString()
+			<< " and " << destination.ToString() << " is "
+			<< distance << ". " << std::endl;
+	}
+
+	catch (std::exception& ex)
+	{
+		std::cerr << ex.what() << " Ending the program." << std::endl;
+	}
+
+	// done.
+	std::cout << std::endl << std::endl;
+	return 0;
+} // end of main()
